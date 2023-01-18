05a88dd9db708f1802d59819f944d5a639570891
Sanjana Paye <paye@usc.edu>
2023-01-18 08:09:16 +0000

    done with pt 1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,47 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+using namespace std;
 
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  if(in != NULL){
+    Node* current = in;
+    in = current -> next;
+    if((current->value)%2 != 0){
+      if(odds == NULL){
+        // odds->value = current->value;
+        // odds->next = current->next;
+        odds = current;
+        odds -> next = NULL;
+      }else{
+        Node* temp = odds;
+        // odds -> value = current->value;
+        // odds -> next = temp;
+        current->next = temp;
+        odds = current;
+      }
+    }else{
+      if(evens == NULL){
+        // evens->value = current->value;
+        // evens->next = current -> next;
+        evens = current;
+        evens -> next = NULL;
+      }else{
+        Node* temp = evens;
+        // evens -> value = current->value;
+        // evens -> next = temp;
+        current->next = temp;
+        evens = current;
+      }
+    }
+    split(in, odds, evens);
+  }
+  
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,54 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+  Node* head = NULL;
+  Node* odds = NULL;
+  Node* evens = NULL;
+  Node* firstnode = new Node(10, NULL);
+  head = firstnode;
+  for (int i = 9; i > 0; i-=2){
+    Node* temp = head;
+    Node* newnode = new Node(i, temp);
+    // newnode -> value = i;
+    // newnode ->next = temp;
+    head = newnode;
+  } 
+  Node*temp = head;
+  while (temp != NULL){
+    cout << temp->value << endl;
+    temp = temp->next;
+  }
+  split(head, odds, evens);
+  Node*temp2 = odds;
+  while (temp2 != NULL){
+    cout << temp2->value << endl;
+    temp2 = temp2->next;
+  }
+  Node*temp3 = evens;
+  while (temp3 != NULL){
+    cout << temp3->value << endl;
+    temp3 = temp3->next;
+  }
 
+  while (head != NULL){
+    Node* temp = head;
+    head = temp->next;
+    delete temp;
+  }
+  while (odds != NULL){
+    Node* temp = odds;
+    odds = temp->next;
+    delete temp;
+  }
+  while (evens != NULL){
+    Node* temp = evens;
+    evens = temp->next;
+    delete temp;
+  }
 }
